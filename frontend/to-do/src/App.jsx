1f905761b7a47bd3dba9634b80a7499c228e538b
BARAKA47 <ahmedhamdy2517@gmail.com>
2024-10-18 15:28:38 +0300

    trial

--- frontend/to-do/src/App.jsx
+++ frontend/to-do/src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./pages/Login/Login";
 const routes = (
   <Router>
     <Routes>
-      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<SignUp />} />
     </Routes>
